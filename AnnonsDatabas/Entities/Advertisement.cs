ff409fd599a7bd19de9d98eb901a1b92bc235592
Dennis <dennisbircan@gmail.com>
2024-11-01 11:22:01 +0100

    eventuellt ändra till classer om jag orkar

--- AnnonsDatabas/Entities/Advertisement.cs
+++ AnnonsDatabas/Entities/Advertisement.cs
@@ -6,11 +6,11 @@
         public string Title { get; set; }
         public string AdDescription { get; set; }
         public Decimal Price { get; set; }
-        public int CategoryId { get; set; }
-        public int CreatedBy { get; set; }
+        Category CategoryId { get; set; }
+        Account CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
 
-        public Advertisement(int id, string title, string adDescription, decimal price, int categoryId, int createdBy, DateTime createdDate)
+        public Advertisement(int id, string title, string adDescription, decimal price, Category categoryId, Account createdBy, DateTime createdDate)
         {
             Id = id;
             Title = title;
